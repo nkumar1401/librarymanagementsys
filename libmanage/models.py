b9692f49e0da47345cb3518f5cd761a79914a458
Nirmal Kumar Bhagatkar <nkbhagatkar@gmail.com>
2025-07-23 13:14:45 +0530

    updates

--- libmanage/models.py
+++ libmanage/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from decouple import config
+from django.contrib.auth import password_validation
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class User(AbstractUser):
@@ -59,9 +61,10 @@ from django.core.validators import MaxValueValidator ,MinValueValidator
 class BookReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
-    rating = models.IntegerField(MinValueValidator(1),MaxValueValidator(5))
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f' {self.user.username}'
+
--- librarymanagementsys/settings.py
+++ librarymanagementsys/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config 
+from  decouple import config 
 DEBUG=config('DEBUG',default=False ,cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
